f8d054c2123f1eda435705941a8f7641931ce656
Thomas Kottke <t.kottke90@gmail.com>
2024-06-17 13:04:12 -0400

    feat: add troubleshooting folder js

---
+++ dist/scripts/troubleshootingSetup.js
@@ -0,0 +1,16 @@
+/**
+ * Templater Script for Setting up the Title & Filename of a troubleshooting file
+ * @param {*} tp Templater instance
+ * @param {string} prompt Prompt to be passed to the user 
+ * @returns 
+ */
+module.exports = async (tp, prompt) => {
+  const response = await tp.system.prompt(prompt, null, true, false);
+
+  if (!response) {
+    throw Error('Empty Titles Are Not Allowed')
+  }
+
+  await tp.file.rename(`${tp.date.now("YYYYMMDD")} - ${response}`)
+  return `Troubleshooting - ${response}`
+}
